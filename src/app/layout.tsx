29117a9efa7cb61eb377054f03729263955c0d5b
Julian <devphcst@gmail.com>
2026-05-22 17:03:26 +0900

    Add Google site verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export const metadata: Metadata = {
     googleBot: { index: true, follow: true },
   },
   verification: {
+    google: "_5jFsOlN8J3ZlU3HFS0BtZ0y3Oa8e2hDNtFT6aCb-dA",
     other: {
       "naver-site-verification": "80065ee72c07be122150434f535e3da96f016ff3",
     },
